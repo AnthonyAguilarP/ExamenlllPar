0d80024ff4089ee5188289ad4eab123bac0677de
AnthonyAguilarP <109875189+AnthonyAguilarP@users.noreply.github.com>
2022-12-10 15:57:09 -0600

    Add files via upload

---
+++ Examen/src/examen/User.java
@@ -0,0 +1,35 @@
+package examen;
+public class User {
+   private String nombre, id, contra;
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getContra() {
+        return contra;
+    }
+
+    public void setContra(String contra) {
+        this.contra = contra;
+    }
+
+    public User(String nombre, String id, String contra) {
+        this.nombre = nombre;
+        this.id = id;
+        this.contra = contra;
+    }
+   
+}
